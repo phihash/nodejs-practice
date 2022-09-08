c159b7ef5a393ddd329bc01b1cc4d5f4cdf8b350
phihash <insharp0220@gmail.com>
2022-09-08 23:28:10 +0900

    基本のコード

---
+++ chapter1.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const server = http.createServer();
+server.on("request",doRequest);
+server.listen(1234);
+console.log("Server Runnning");
+function doRequest(req,res){
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  res.write("送りたいテキスト")
+  res.end();
+}
