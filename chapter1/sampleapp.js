733e7a642f505a065e1e43407046d26d460aae93
phihash <insharp0220@gmail.com>
2022-09-21 01:00:55 +0900

    Node.js利用の準備を整えよう

---
+++ chapter1/sampleapp.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const server = http.createServer();
+
+server.on("request",doRequest);
+server.listen(1234);
+// server.listen(process.env.PORT, process.env.IP);
+console.log("Server Runnning");
+
+function doRequest(req,res){
+  res.writeHead(200,{'Content-Type': 'text/plain'});
+  res.write("Hello World");
+  res.end();
+}
